a22c59dbb2fc9e03764e5ae6fcfeb59119527bdf
axelpoin7-sudo <axelpoin7@gmail.com>
2026-02-22 19:38:11 -0400

    Final: Integración y validación de datos

---
+++ RegistroOngApp/DatabaseConnection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+
+public sealed class DatabaseConnection
+{
+    private static DatabaseConnection _instance;
+    private readonly string _connectionString ="Server=TU_SERVIDOR;Database=OngDB;Integrated Security=True;TrustServerCertificate=True;"; 
+    public SqlConnection Connection { get; private set; }
+
+    // Constructor privado: previene instanciación externa 
+    private DatabaseConnection()
+    {
+        Connection = new SqlConnection(_connectionString);
+    }
+
+    // Punto de acceso global único 
+    public static DatabaseConnection Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new DatabaseConnection();
+            }
+            return _instance;
+        }
+    }
+}
+
